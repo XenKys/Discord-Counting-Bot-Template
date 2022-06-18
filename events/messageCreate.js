2a7efa23065704e473a2f3d8ccd632e98eab8871
XenKys <sergiogotuzzo040309@gmail.com>
2022-06-18 20:17:01 +0200

    Discord Counting Bot Template

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,39 +6,46 @@ client.on("messageCreate", async (message) => {
 
     const countingData = await countingModel.find().catch((err) => console.log(err))
 
-    if (!countingData) return
-
-    if (message.author.id == countingData.lastUserID) {
-        message.reply({
-            content: "Each user can write only one number at a time"
-        })
-        message.react("❌")
-
-        message.channel.send("0").then((msg) => msg.react("✅"))
-
-        countingData.lastNumber = 0
-        countingData.lastUserID = client.user.id
-        countingData.save()
-        return
-    }
-
-    if (message.content == countingData.lastNumber + 1) {
+    if (countingData) {
+        if (message.author.id == countingData.lastUserID) {
+            message.reply({
+                content: "Each user can write only one number at a time"
+            })
+            message.react("❌")
+    
+            message.channel.send("0").then((msg) => msg.react("✅"))
+    
+            countingData.lastNumber = 0
+            countingData.lastUserID = client.user.id
+            countingData.save()
+            return
+        }
+    
+        if (message.content == countingData.lastNumber + 1) {
+            message.react("✅")
+    
+            countingData.lastNumber = parseInt(message.content)
+            countingData.lastUserID = message.author.id
+            countingData.save()
+        } else {
+            message.reply({
+                content: `The number you entered is incorrect, you should have entered ${countingData.lastNumber + 1}`
+            })
+            message.react("❌")
+    
+            message.channel.send("0").then((msg) => msg.react("✅"))
+    
+            countingData.lastNumber = 0
+            countingData.lastUserID = client.user.id
+            countingData.save()
+            return
+        }
+    } else {
         message.react("✅")
 
-        countingData.lastNumber = message.content
-        countingData.lastUserID = message.author.id
-        countingData.save()
-    } else {
-        message.reply({
-            content: `The number you entered is incorrect, you should have entered ${countingData.lastNumber + 1}`
-        })
-        message.react("❌")
-
-        message.channel.send("0").then((msg) => msg.react("✅"))
-
-        countingData.lastNumber = 0
-        countingData.lastUserID = client.user.id
-        countingData.save()
-        return
+        new countingModel({
+            lastNumber: parseInt(message.content),
+            lastUserID: message.author.id
+        }).save()
     }
 })
